fa54ad8cc87543f0e1112be431a5ac5e6bd0857a
ken <redcool007@qq.com>
2022-07-28 18:04:54 +0800

    update TangentLib.hlsl

--- Lib/TangentLib.hlsl
+++ Lib/TangentLib.hlsl
@@ -21,6 +21,14 @@
     output.tSpace1 = float4(t.y,b.y,n.y,p.y);\
     output.tSpace2 = float4(t.z,b.z,n.z,p.z)
 
+#define TANGENT_SPACE_COMBINE_WORLD(vertex/*float3*/,normal/*float3*/,tangent/*float4*/,output/*{float4 tSpace[0..2]}*/)\
+    float3 p = vertex;\
+    float3 n = normal;\
+    float3 t = tangent.xyz;\
+    float3 b = normalize(cross(n,t) * tangent.w);\
+    output.tSpace0 = float4(t.x,b.x,n.x,p.x);\
+    output.tSpace1 = float4(t.y,b.y,n.y,p.y);\
+    output.tSpace2 = float4(t.z,b.z,n.z,p.z)
 /**
     split input.tSpace[0..2] to
     float3 tangent,binormal,normal,worldPos 
@@ -31,11 +39,6 @@
     float3 normal = normalize(float3(input.tSpace0.z,input.tSpace1.z,input.tSpace2.z));\
     float3 worldPos = (float3(input.tSpace0.w,input.tSpace1.w,input.tSpace2.w))
 
-float3 TangentToWorld(float3 tSpace0,float3 tSpace1,float3 tSpace2,float3 tn){
-    return normalize(float3(dot(tSpace0,tn),dot(tSpace1,tn),dot(tSpace2,tn)));
-}
+#define TangentToWorld(tn,tSpace0,tSpace1,tSpace) normalize(float3(dot(tSpace0.xyz,tn),dot(tSpace1.xyz,tn),dot(tSpace2.xyz,tn)))
 
-float3 TangentToWorld(float4 tSpace0,float4 tSpace1,float4 tSpace2,float3 tn){
-    return normalize(float3(dot(tSpace0.xyz,tn),dot(tSpace1.xyz,tn),dot(tSpace2.xyz,tn)));
-}
 #endif //TANGENT_LIB_HLSL
