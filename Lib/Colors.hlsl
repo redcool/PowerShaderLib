21a5d7fca7eb90f5ccdfc2f1ab2fc900308fc76e
ken <redcool007@qq.com>
2022-12-16 16:14:04 +0800

    v(0.0.2.1) update Colors.hlsl

--- Lib/Colors.hlsl
+++ Lib/Colors.hlsl
@@ -11,15 +11,21 @@ half3 ThinFilm(half invertNV,half scale,half offset,half saturate,half brightnes
     return HsvToRgb(half3(h,s,v));
 }
 
-float3 ColorGradingExposure(float3 c,float m){
-    return c*m;
+float3 Luminance(float3 c,bool useACES){
+    return useACES ? AcesLuminance(c) : Luminance(c);
 }
-float3 ColorGradingContrast(float3 c,float m){
-    c = LinearToLogC(c);
-    c = (c - ACEScc_MIDGRAY) * m + ACEScc_MIDGRAY;
-    c = LogCToLinear(c);
-    return max(0,c);
+
+float3 ColorGradingExposure(float3 c,float scale){
+    return c*scale;
 }
+
+float3 ColorGradingContrast(float3 c,float scale,bool useACES){
+    c = useACES ? ACES_to_ACEScc(unity_to_ACES(c)) : LinearToLogC(c);
+    c = (c - ACEScc_MIDGRAY) * scale + ACEScc_MIDGRAY;
+    c = useACES ? ACES_to_ACEScg(ACEScc_to_ACES(c)) : LogCToLinear(c);
+    return c;
+}
+
 float3 ColorGradingFilter(float3 c,float3 filter){
     return c * filter.xyz;
 }
@@ -38,18 +44,56 @@ float3 ColorGradingWhiteBalance(float3 c,float3 whiteBalanceColor){
     return LMSToLinear(c);
 }
 
+float3 ColorGradingSplitToning(float3 c,float3 shadows,float3 highlights,float balance,bool useACES){
+    c = PositivePow(c,1/2.2);
+
+    float t = saturate(Luminance(c,useACES) + balance);
+    shadows = lerp(0.5,shadows, 1 - t);
+    highlights = lerp(0.5,highlights, t);
+
+    c = SoftLight(c,shadows);
+    c = SoftLight(c,highlights);
+    return PositivePow(c,2.2);
+}
+
+float3 ColorGradingChannelMixer(float3 c,float3 red,float3 green,float3 blue){
+    return mul(float3x3(red,green,blue),c);
+}
+
+float3 ColorGradingShadowsMidtonesHighlights(float3 c,float3 shadowColor,float3 midtoneColor,float3 highlightColor,float4 range,bool useACES){
+    float luma = Luminance(c,useACES);
+    float shadowsWeight = 1 - smoothstep(range.x,range.y,luma);
+    float highlightsWeight = smoothstep(range.z,range.w,luma);
+    float midtonesWeight = 1 - shadowsWeight - highlightsWeight;
+    return c * shadowColor * shadowsWeight +
+        c * midtoneColor * midtonesWeight + 
+        c * highlightColor * highlightsWeight;
+}
+
 float4 _ColorAdjustments; // {x:exposure,y:constrast}
 float4 _ColorFilter; //{xyz:rgb}
 float4 _ColorAdjustHSV; //{xy:hue scale,offset,z:saturate,w:value}
 float4 _WhiteBalanceFactors;
-float3 ColorGrading(float3 c){
+float4 _SplitToningHighlights,_SplitToningShadows;//{xyz:rgb,w:balance}
+float4 _ChannelMixerRed,_ChannelMixerGreen,_ChannelMixerBlue;
+
+float4 _SMHShadows,_SMHMidtones,_SMHHighlights,_SMHRange;
+float3 ColorGrading(float3 c,bool useACES=false){
     c = min(60,c);
     c = ColorGradingExposure(c,_ColorAdjustments.x);
     c = ColorGradingWhiteBalance(c,_WhiteBalanceFactors.xyz);
-    c = ColorGradingContrast(c,_ColorAdjustments.y);
+    c = ColorGradingContrast(c,_ColorAdjustments.y,useACES);
     c = ColorGradingFilter(c,_ColorFilter);
+    c = max(c,0);
+
+    c = ColorGradingSplitToning(c,_SplitToningShadows.xyz,_SplitToningHighlights.xyz,_SplitToningShadows.w,useACES);
+    c = ColorGradingChannelMixer(c,_ChannelMixerRed.xyz,_ChannelMixerGreen.xyz,_ChannelMixerBlue.xyz);
+    c = max(c,0);
+
+    c = ColorGradingShadowsMidtonesHighlights(c,_SMHShadows,_SMHMidtones,_SMHHighlights,_SMHRange,useACES);
+
     c = ColorGradingHSV(c,_ColorAdjustHSV.xy,_ColorAdjustHSV.z,_ColorAdjustHSV.w);
-    return max(0,c);
+    return max(useACES ? ACEScg_to_ACES(c) : c,0);
 }
 
 #endif //COLORS_HLSL
