ff9d015208c10549d452d55c37e2076ecfe3859d
ken <redcool007@qq.com>
2023-03-23 14:27:58 +0800

    v(0.0.3.12) MaskLib.hlsl add GetUIMask

--- Lib/MaskLib.hlsl
+++ Lib/MaskLib.hlsl
@@ -25,4 +25,17 @@ half GetMask(half4 maskData,half maskFrom){
     return maskData[maskFrom];
 }
 
+/**
+    unity gui mask
+*/
+half4 GetUIMask(float4 vertex,float hclipPosW,float4 clipRect,float2 uiMaskSoftness){
+    float2 pixelSize = hclipPosW;
+    pixelSize /= abs(mul((float2x2)UNITY_MATRIX_P,_ScreenParams.xy));
+
+    float4 clampedRect = clamp(clipRect,-2e10,2e10);
+    float2 maskUV = (vertex.xy - clampedRect.xy)/(clampedRect.zw - clampedRect.xy);
+    float4 mask = half4(vertex.xy*2 - clampedRect.xy - clampedRect.zw, 0.25/(0.25*half2(uiMaskSoftness)+abs(pixelSize)) );
+    return mask;
+}
+
 #endif //MASK_LIB_HLSL
