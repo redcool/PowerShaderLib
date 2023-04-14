2be12abce0b7752e57e3fb24d3230dbe661ad8df
ken <redcool007@qq.com>
2023-04-14 17:32:08 +0800

    v(0.0.3.19) update CameraTextures.hlsl

--- Lib/ScreenTextures.hlsl
+++ Lib/ScreenTextures.hlsl
@@ -1,10 +1,10 @@
-#if !defined(CAMERA_TEXTURES_HLSL)
-#define CAMERA_TEXTURES_HLSL
+#if !defined(SCREEN_TEXTURES_HLSL)
+#define SCREEN_TEXTURES_HLSL
 
 #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/DeclareOpaqueTexture.hlsl"
 #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/DeclareDepthTexture.hlsl"
 
-float SampleCameraDepthTexture(float2 suv){
+float GetScreenDepth(float2 suv){
     float depth = SAMPLE_TEXTURE2D(_CameraDepthTexture,sampler_CameraDepthTexture,UnityStereoTransformScreenSpaceTex(suv)).x;
 
     #if !defined(UNITY_REVERSED_Z)
@@ -13,8 +13,12 @@ float SampleCameraDepthTexture(float2 suv){
     return depth;
 }
 
-float3 SampleCameraOpaqueTexture(float2 suv){
+float3 CalcWorldNormal(float3 worldPos){
+    return normalize(cross(ddy(worldPos),ddx(worldPos)));
+}
+
+float3 GetScreenColor(float2 suv){
     return  SAMPLE_TEXTURE2D_X(_CameraOpaqueTexture, sampler_CameraOpaqueTexture, UnityStereoTransformScreenSpaceTex(suv)).xyz;
 }
 
-#endif //CAMERA_TEXTURES_HLSL
+#endif //SCREEN_TEXTURES_HLSL
