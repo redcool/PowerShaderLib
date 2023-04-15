868209ebc0c42c40014e42df17ee00a7f7fabe52
ken <redcool007@qq.com>
2023-04-15 20:44:24 +0800

    v(0.0.3.20) add BlurLib.hlsl

---
+++ Lib/BlurLib.hlsl
@@ -0,0 +1,82 @@
+#if !defined(BLUR_LIB_HLSL)
+#define BLUR_LIB_HLSL
+
+#include "Packages/com.unity.render-pipelines.core/ShaderLibrary/Common.hlsl"
+//----------------
+// Box blur
+//----------------
+float4 SampleBox(TEXTURE2D_PARAM(tex,state), float4 texel, float2 uv, float delta) {
+	float2 p = texel.xy * delta;
+	float4 c = SAMPLE_TEXTURE2D(tex, state, uv + float2(-1, -1) * p);
+	c += SAMPLE_TEXTURE2D(tex, state, uv + float2(1, -1) * p);
+	c += SAMPLE_TEXTURE2D(tex, state, uv + float2(-1, 1) * p);
+	c += SAMPLE_TEXTURE2D(tex, state, uv + float2(1, 1) * p);
+
+	return c * 0.25;
+}
+
+float4 SampleBox(TEXTURE2D_PARAM(tex,state),float4 texel,float2 uv, float delta,float sideWeight,float centerWeight=0) {
+    float2 p = texel.xy * delta;
+    float4 c = SAMPLE_TEXTURE2D(tex,state,uv + float2(-1,-1) * p) * sideWeight;
+    c += SAMPLE_TEXTURE2D(tex,state,uv + float2(1,-1) * p) * sideWeight;
+    c += SAMPLE_TEXTURE2D(tex,state,uv + float2(-1,1) * p) * sideWeight;
+    c += SAMPLE_TEXTURE2D(tex,state,uv + float2(1,1) * p) * sideWeight;
+    //c += SAMPLE_TEXTURE2D(tex,state,uv) * centerWeight;
+    return c;
+}
+
+//----------------
+// Gaussian blur
+//----------------
+const static float gaussWeights[4]={0.00038771,0.01330373,0.11098164,0.22508352};
+
+float4 GaussBlur(TEXTURE2D_PARAM(tex,sampler_tex),float2 uv,float2 offset,bool samplerCenter){
+    float4 c = 0;
+    if(samplerCenter){
+        c += SAMPLE_TEXTURE2D(tex,sampler_tex,uv) * gaussWeights[3];
+    }
+    c += SAMPLE_TEXTURE2D(tex,sampler_tex,uv + offset) * gaussWeights[2];
+    c += SAMPLE_TEXTURE2D(tex,sampler_tex,uv - offset) * gaussWeights[2];
+
+    c += SAMPLE_TEXTURE2D(tex,sampler_tex,uv + offset * 2) * gaussWeights[1];
+    c += SAMPLE_TEXTURE2D(tex,sampler_tex,uv - offset * 2) * gaussWeights[1];
+
+    c += SAMPLE_TEXTURE2D(tex,sampler_tex,uv + offset * 3) * gaussWeights[0];
+    c += SAMPLE_TEXTURE2D(tex,sampler_tex,uv - offset * 3) * gaussWeights[0];
+    return c;
+}
+
+float4 SampleGaussian(TEXTURE2D_PARAM(tex,state), float2 texel, float2 uv) {
+	float4 c = GaussBlur(tex,state,uv,float2(texel.x,0),true);
+	c += GaussBlur(tex,state,uv,float2(0,texel.y),true);
+	return c;
+}
+
+
+//----------------
+// Dir blur
+//----------------
+#define DIR_BLUR_SAMPLES 6
+const static float dirBlurWeights[DIR_BLUR_SAMPLES] = {-0.03,-0.02,-0.01,0.01,0.02,0.03};
+float4 SampleDirBlur(TEXTURE2D_PARAM(tex,state),float2 uv,float2 dir){
+    float4 c = 0;
+    for(int i=0;i<DIR_BLUR_SAMPLES;i++){
+        c += SAMPLE_TEXTURE2D(tex,state,(uv + dir * dirBlurWeights[i]));
+    }
+    return c / (DIR_BLUR_SAMPLES);
+}
+
+
+//----------------
+// Kawase blur
+//----------------
+float4 KawaseBlur(TEXTURE2D_PARAM(tex,state),float2 uv,float2 texelSize,float blurSize){
+    const half2 offsets[4] = {-1,-1, 1,1, -1,1, 1,-1};
+    float4 c = 0;
+    for(int x=0;x<4;x++){
+        c += SAMPLE_TEXTURE2D(tex,state,uv + offsets[x] * texelSize * blurSize);
+    }
+    c *= 0.25;
+    return c;
+}
+#endif
---
+++ Test/TestBlur.shader
@@ -0,0 +1,99 @@
+Shader "Unlit/TestBlur"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" {}
+        _Size("_Size",float) = 1
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+
+        Pass
+        {
+            
+            HLSLPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
+            #include "../Lib/BlurLib.hlsl"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
+
+            TEXTURE2D(_MainTex);SAMPLER(sampler_MainTex);
+            float4 _MainTex_ST;
+            float4 _MainTex_TexelSize;
+            float _Size;
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = TransformObjectToHClip(v.vertex);
+                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+                return o;
+            }
+
+            half4 frag (v2f i) : SV_Target
+            {
+                float4 c = GaussBlur(_MainTex,sampler_MainTex,i.uv,float2(_MainTex_TexelSize.x,0) * _Size,true);
+                
+                return c*float4(1,0,0,1);
+            }
+            ENDHLSL
+        }
+
+        pass
+        {
+            Tags{"LightMode"="UniversalForward"}
+            HLSLPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
+            #include "../Lib/BlurLib.hlsl"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
+
+            TEXTURE2D(_MainTex);SAMPLER(sampler_MainTex);
+            float4 _MainTex_ST;
+            float4 _MainTex_TexelSize;
+            float _Size;
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = TransformObjectToHClip(v.vertex);
+                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+                return o;
+            }
+
+            half4 frag (v2f i) : SV_Target
+            {
+                // float4 c = GaussBlur(_MainTex,sampler_MainTex,i.uv,float2(0,_MainTex_TexelSize.y) * _Size,true);
+                float4 c = SampleGaussian(_MainTex,sampler_MainTex,_MainTex_TexelSize.xy*_Size,i.uv);
+                return c*float4(0,1,0,1);
+            }
+            ENDHLSL
+        }
+    }
+}
