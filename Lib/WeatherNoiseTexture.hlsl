b46a3da4ff39f20bcc8bc8d0883858bee072661c
gamebox1 <redcool007@qq.com>
2023-11-11 19:56:05 +0800

    v(0.0.4.62) add comments

--- Lib/WeatherNoiseTexture.hlsl
+++ Lib/WeatherNoiseTexture.hlsl
@@ -1,11 +1,22 @@
+/***
+    
+    SIMPLE_NOISE_TEX
+
+*/
+
+
 #if !defined(WEATHER_NOISE_TEX_HLSL)
 // noise4Layer texture
 TEXTURE2D(_WeatherNoiseTexture);SAMPLER(sampler_WeatherNoiseTexture);
-
+/**
+    get noise,return [-,1]
+*/
 float SampleWeatherNoise(float2 uv,half4 ratio=half4(.5,.25,.0125,.063)){
     float4 n4 = SAMPLE_TEXTURE2D(_WeatherNoiseTexture,sampler_WeatherNoiseTexture,uv*0.1);
     // simple version
-    // return n4.w*2-1;
+    #if defined(SIMPLE_NOISE_TEX)
+    return n4.w*2-1;
+    #endif
     // full version
     float n = dot(n4,ratio);
     n = n*2-1;
