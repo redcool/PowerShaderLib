0447a1574805403f6403a01ea217526ed7a67289
gamebox1 <redcool007@qq.com>
2024-01-11 18:09:17 +0800

    v(0.0.5.23) simplify

--- Lib/BigShadows.hlsl
+++ Lib/BigShadows.hlsl
@@ -3,14 +3,14 @@
 #include "UnityLib.hlsl"
 
 #define _MainLightShadowmapSize _BigShadowMap_TexelSize
-#define SHADOW_INTENSITY _BigShadowParams.x
+#define SHADOW_INTENSITY saturate(_BigShadowParams.x)
 
 
 float4 _BigShadowMap_TexelSize;
 #include "ShadowsLib.hlsl"
 
+TEXTURE2D_SHADOW(_BigShadowMap); SAMPLER_CMP(sampler_BigShadowMap);
 
-TEXTURE2D(_BigShadowMap); SAMPLER_CMP(sampler_BigShadowMap);
 float4x4 _BigShadowVP;
 float4 _BigShadowParams; //{x: shadow intensity}
 
@@ -19,20 +19,22 @@ float3 TransformWorldToBigShadow(float3 worldPos){
     return bigShadowCoord;
 }
 
+#define BEYOND_SHADOW_FAR(shadowCoord) shadowCoord.z <= 0.0 || shadowCoord.z >= 1.0
+
 /**
     _SHADOWS_SOFT for soft shadow 
 */
 float CalcBigShadowAtten(float3 bigShadowCoord,float softScale){
-    branch_if(SHADOW_INTENSITY<=0)
-        return 1;
+    // branch_if(SHADOW_INTENSITY<=0)
+    //     return 1;
     
-    branch_if(any(bigShadowCoord <= 0) || any(bigShadowCoord >= 1))
-        return 1;
+    // branch_if(any(bigShadowCoord <= 0) || any(bigShadowCoord >= 1))
+    //     return 1;
     
-    float shadow = SampleShadowmap(_BigShadowMap,sampler_BigShadowMap,bigShadowCoord.xyzx,softScale);
+    float shadow = SampleShadowmap(TEXTURE2D_ARGS(_BigShadowMap,sampler_BigShadowMap),bigShadowCoord.xyzx,softScale);
     shadow = lerp(1,shadow,SHADOW_INTENSITY);
     
-    return shadow;
+    return BEYOND_SHADOW_FAR(bigShadowCoord) ? 1 : shadow;
 }
 
 #endif // BIG_SHADOWS_HLSL
