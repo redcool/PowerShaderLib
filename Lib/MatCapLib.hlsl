8c89685d9419298f2305bf73e70afe64ef427a15
gamebox1 <redcool007@qq.com>
2024-04-10 16:31:15 +0800

    v(0.0.5.49) add rotAngle

--- Lib/MatCapLib.hlsl
+++ Lib/MatCapLib.hlsl
@@ -1,11 +1,21 @@
 #if !defined(MAT_CAP_LIB_HLSL)
 #define MAT_CAP_LIB_HLSL
 
-half4 SampleMatCap(TEXTURE2D_PARAM(matcapTex,samplerMatCap),float3 normal,float4 mapCap_ST){
+#include "MathLib.hlsl"
+
+/**
+    keyword : 
+    MATCAP_UV_ROTATE
+*/
+half4 SampleMatCap(TEXTURE2D_PARAM(matcapTex,samplerMatCap),float3 normal,float4 mapCap_ST,float rotAngle=0){
     float3 normalView = mul(UNITY_MATRIX_V,float4(normal,0)).xyz;
     normalView = normalView*0.5+0.5;
 
     float2 matUV = (normalView.xy) * mapCap_ST.xy + mapCap_ST.zw;
+    #if defined(MATCAP_UV_ROTATE)
+        RotateUV(rotAngle,0.5,matUV/**/);
+    #endif
+
     float4 matCap = SAMPLE_TEXTURE2D(matcapTex,samplerMatCap,matUV);
     return matCap;
 }
