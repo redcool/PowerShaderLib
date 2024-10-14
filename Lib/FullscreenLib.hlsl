633cdefb27a6f027ce21d748c3cb03fc57a730d4
gamebox1 <redcool007@qq.com>
2024-10-14 17:55:30 +0800

    v(0.0.7.10) add FullscreenLib.hlsl

---
+++ Lib/FullscreenLib.hlsl
@@ -0,0 +1,22 @@
+#if !defined(FULLSCREEN_LIB_HLSL)
+#define FULLSCREEN_LIB_HLSL
+
+/**
+    remap ndc[-1,1] to new screen pos
+*/
+float2 RemapNdcToScreenPos(float2 ndcPos/*[-1,1]*/,float4 screenPosRange/*[0,1]*/){
+    float4 range = screenPosRange * 2- 1;// xz:x range,yw:y range
+    float2 ndc01 = ndcPos * 0.5 + 0.5;
+    return lerp(range.xy,range.zw,ndc01);
+}
+
+/**
+    Transform object to ndc[-1,1] or hclip
+    screenPosRange : [0,1]
+*/
+float4 TransformObjectToNdcHClip(float4 vertex,bool isNdc,float4 screenPosRange){
+    float4 ndcPos = float4(vertex.xy*2,0,1); // [-0.5,0.5] ,like unity cube ,quad
+    ndcPos.xy = RemapNdcToScreenPos(ndcPos.xy,screenPosRange) ;
+    return isNdc ? ndcPos : TransformObjectToHClip(vertex.xyz);
+}
+#endif //FULLSCREEN_LIB_HLSL
