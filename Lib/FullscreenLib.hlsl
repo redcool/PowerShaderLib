70d3f48e709a7802ceb2be8e4cfc111f5a350c4c
gamebox1 <redcool007@qq.com>
2025-07-18 18:31:44 +0800

    v(0.0.8.56) udpate FullscreenLib.hlsl

--- Lib/FullscreenLib.hlsl
+++ Lib/FullscreenLib.hlsl
@@ -14,8 +14,9 @@ float2 RemapNdcToScreenPos(float2 ndcPos/*[-1,1]*/,float4 screenPosRange/*[0,1]*
     Transform object to ndc[-1,1] or hclip
     screenPosRange : default [0,0,1,1]
 */
-float4 TransformObjectToNdcHClip(float4 vertex,bool isNdc,float4 screenPosRange){
-    float4 ndcPos = float4(vertex.xy*2,UNITY_NEAR_CLIP_VALUE,1); // [-0.5,0.5] ,like unity cube ,quad
+float4 TransformObjectToNdcHClip(float4 vertex,bool isNdc,float4 screenPosRange,bool isUseUV1=false,float2 uv1=0){
+    float2 suv = isUseUV1 ? uv1 - 0.5: vertex.xy;
+    float4 ndcPos = float4(suv*2,UNITY_NEAR_CLIP_VALUE,1); // [-0.5,0.5] ,like unity cube ,quad
     ndcPos.xy = RemapNdcToScreenPos(ndcPos.xy,screenPosRange) ;
     return isNdc ? ndcPos : TransformObjectToHClip(vertex.xyz);
 }
