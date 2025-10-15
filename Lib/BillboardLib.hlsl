f348ec1c90782c25d680d10b316e6730b35b68bb
gamebox1 <redcool007@qq.com>
2025-10-15 20:25:09 +0800

    v(0.0.8.73) update: BillboardLib.hlsl

--- Lib/BillboardLib.hlsl
+++ Lib/BillboardLib.hlsl
@@ -1,13 +1,56 @@
+/**
+
+*/
+
 #if !defined(BILLBOARD_LIB_HLSL)
 #define BILLBOARD_LIB_HLSL
 
 
-float3 CalcPosOffsetViewSpace(float3 vertex,bool fullFaceCamera){
-    float3x3 camRotMat = float3x3(
-        UNITY_MATRIX_V[0].xyz,
-        UNITY_MATRIX_V[1].xyz,
-        UNITY_MATRIX_V[2].xyz
+#if defined(ROTATE_X_AXIS) || defined(ROTATE_Y_AXIS) || defined(ROTATE_Z_AXIS)
+#define ROTATE_AXIS_ON 
+#endif
+
+/**
+    get rotated matrix (x,y,z)
+    3 keywords for 3 axis
+
+    ROTATE_X_AXIS : x
+    ROTATE_Y_AXIS : y
+    ROTATE_Z_AXIS : z
+*/
+float3x3 GetRotMat(half rotDegree){
+    float2 cs = (float2)0;
+    sincos(radians(rotDegree),cs.y,cs.x);
+
+    #if defined(ROTATE_X_AXIS)
+    return float3x3(
+        1,0,0,
+        0,cs.x,-cs.y,
+        0,cs.y,cs.x
     );
+    #elif defined(ROTATE_Y_AXIS)
+    return float3x3(
+        cs.x,0,cs.y,
+        0,1,0,
+        -cs.y,0,cs.x
+    );
+    #elif defined(ROTATE_Z_AXIS)
+    return float3x3(
+        cs.x,-cs.y,0,
+        cs.y,cs.x,0,
+        0,0,1
+    );
+    #endif
+    return float3x3(1,0,0,0,1,0,0,0,1);
+}
+/**
+    calc vertex (scale,rotate) in view space
+*/
+float3 CalcPosOffsetViewSpace(float3 vertex,bool isFullFaceCamera,half rotDegree=0){
+    float3x3 camRotMat = (float3x3)UNITY_MATRIX_V;
+    #if defined(ROTATE_AXIS_ON)
+    camRotMat = mul(GetRotMat(rotDegree),camRotMat);
+    #endif
 
     float sx = unity_ObjectToWorld._11;
     float sy = unity_ObjectToWorld._22;
@@ -15,18 +58,16 @@ float3 CalcPosOffsetViewSpace(float3 vertex,bool fullFaceCamera){
     float3 vertexOffset = float3(sx,sy,0) * vertex.xyz;
     float3 vertexRotate = mul(camRotMat,float3(0,vertex.y * sy,0));
 
-    if(!fullFaceCamera){
-        vertexOffset.y = 0;
-        vertexOffset += vertexRotate;
-    }
+    vertexOffset = isFullFaceCamera ? vertexOffset : float3(vertexOffset.x,0,vertexOffset.z) + vertexRotate;
+
     return vertexOffset;
 }
 
 /**
     rotate vertex ,face to camera
 */
-float4 TransformBillboardObjectToHClip(float3 vertex,bool fullFaceCamera){
-    float3 vertexOffset = CalcPosOffsetViewSpace(vertex,fullFaceCamera);
+float4 TransformBillboardObjectToHClip(float3 vertex,bool isFullFaceCamera,half rotDegree=0){
+    float3 vertexOffset = CalcPosOffsetViewSpace(vertex,isFullFaceCamera,rotDegree);
     return mul(UNITY_MATRIX_P,
         mul(UNITY_MATRIX_MV,float4(0,0,0,1)) + float4(vertexOffset,1)
     );
